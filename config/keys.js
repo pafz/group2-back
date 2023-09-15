bfaf64a074ecb59685070e9cb4f9e4b53b1484aa
pafz <paferza@gmail.com>
2023-09-15 16:53:07 +0200

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MONGO_URI: 'mongodb+srv://paferza:<password>@cluster0.uw7fvq9.mongodb.net',
+};
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+require('dotenv').config();
+const { dbConnection } = require('./config/config');
+const app = express();
+const cors = require('cors');
+//const { handleTypeError } = require('./middleware/errors');
+const PORT = process.env.PORT || 3001;
+
+dbConnection();
+
+app.use(express.json());
+app.use(cors());
+
+// app.use('/uploads', express.static('uploads'));
+// app.use('/users', require('./routes/users'));
+
+// app.use('/events', require('./routes/events'));
+
+// app.use(handleTypeError);
+
+app.listen(PORT, () => console.log(`Servidor levantado en el puerto ${PORT}`));
+
+module.exports = app;
