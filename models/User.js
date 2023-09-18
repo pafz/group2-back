dc55d98dfcfb141deb991b26bb8d4b0a0643b569
pafz <paferza@gmail.com>
2023-09-18 09:33:25 +0200

    user WIP creating models

--- models/User.js
+++ models/User.js
@@ -1,7 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const UserSchema = new mongoose.Schema({});
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Por favor rellena tu nombre'],
+    },
+    email: { type: String, required: [true, 'Por favor rellena tu email'] },
+    password: {
+      type: String,
+      required: [true, 'Por favor rellena tu contraseña'],
+    },
+    confirmed: {
+      type: Boolean,
+      default: false,
+    },
+    _idDoubt: { type: Schema.Types.ObjectId, ref: 'Doubt' },
+    role: String,
+    tokens: [],
+    avatar: String,
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model('User', UserSchema);
 
